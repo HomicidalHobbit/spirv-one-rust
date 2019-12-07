5d9def9711f4638db6c1eef558b3f7e91304bd4c
Richard Underhill <richard.underhill@outlook.com>
2019-12-07 13:06:52 +0000

    Added Linux libraries

--- build.rs
+++ build.rs
@@ -3,9 +3,25 @@ use std::path::Path;
 
 fn main() {
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-lib=dylib=c++");
+    let libs_path = set_libs();
     println!(
         "cargo:rustc-link-search=native={}",
-        Path::new(&dir).display()
+        Path::new(&dir).join(libs_path).display()
     );
+}
+
+#[cfg(target_os = "macos")]
+fn set_libs() -> String {
+    println!("cargo:rustc-link-lib=dylib=c++");
+    //println!("cargo:rustc-link-lib=dylib=iconv");
+    String::from("macos")
+}
+#[cfg(target_os = "linux")]
+fn set_libs() -> String {
+    println!("cargo:rustc-link-lib=static=stdc++");
+    String::from("linux")
+}
+#[cfg(target_os = "windows")]
+fn set_libs() -> String {
+    String::from("windows")
 }
